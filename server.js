2e55e8b08da1c19f9842ee6d1446de56af0ea2f1
revanth-collab <gurramnagarevanth1@gmail.com>
2025-03-03 16:01:23 +0530

    Fixed bcrypt invalid ELF header issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const mysql = require("mysql2");
 const fs = require("fs");
 const mysql = require("mysql2/promise");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
